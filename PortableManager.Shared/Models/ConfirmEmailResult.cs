34687efcb7ed5537a303d5a1c9f1f0a1abe8e874
SenkoVlad <vlad.senko2011@yandex.ru>
2021-03-31 22:28:46 +0300

    added email confirmation view

---
+++ PortableManager.Shared/Models/ConfirmEmailResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortableManager.Shared.Models
+{
+    public class ConfirmEmailResult
+    {
+        public bool Status { get; set; }
+        public IEnumerable<string> Messages { get; set; }
+    }
+}
